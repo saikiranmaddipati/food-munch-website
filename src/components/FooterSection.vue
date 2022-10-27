85fd1794fae44466ee060424ee59ed56b52e0b1a
saikiranmaddipati <maddipatisaikiran24@gmail.com>
2022-10-27 14:46:45 +0530

    footer section

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footer-background q-pa-md">
+    <div class="row">
+        <div class="col-12 text-center">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/food-munch-logo-light.png"
+              class="food-munch-logo"
+            />
+            <h1 class="footer-heading">orderfood@foodmunch.com</h1>
+            <p class="footer-paragraph">Near Awfin Towers, Kothaguda X roads, Hyderabad</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterSection'
+}
+</script>
+
+<style>
+ .footer-background{
+    background-color: #0d2436;
+ }
+ .food-munch-logo{
+    height: 50px;
+    width: 50px;
+ }
+ .footer-heading{
+    color: #959ead;
+    font-family: "Roboto";
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 1rem;
+ }
+ .footer-paragraph{
+    color: #959ead;
+    font-family: "Roboto";
+    font-weight: bold;
+ }
+</style>
--- src/components/MenuSection.vue
+++ src/components/MenuSection.vue
@@ -1,62 +1,64 @@
 <template>
-  <div class="menu-section">
-    <div class="row">
+  <div>
+    <div class="menu-section q-pa-lg">
+      <div class="row">
         <div class="col-12">
-            <h1 class="menu-section-heading">Menu</h1>
+          <h1 class="menu-section-heading">Menu</h1>
         </div>
         <div class="col-3 row justify-center">
-            <div class="menu-item-card">
-              <img
-                src="https://www.weddingbellcaterer.com/wp-content/uploads/2020/05/Weddingbell-Caterer-Menu-Veg-Starter.jpg"
-                class="menu-item-image" @click="vegStarterItems()"
-              />
-              <h1 class="menu-card-title">Veg Starters</h1>
-              <a href="" class="menu-item-link">
-                View All
-              </a>
-            </div>
+          <div class="menu-item-card">
+            <img
+              src="https://www.weddingbellcaterer.com/wp-content/uploads/2020/05/Weddingbell-Caterer-Menu-Veg-Starter.jpg"
+              class="menu-item-image"
+              @click="vegStarterItems()"
+            />
+            <h1 class="menu-card-title">Veg Starters</h1>
+            <a href="" class="menu-item-link"> View All </a>
+          </div>
         </div>
         <div class="col-3 row justify-center">
-            <div class="menu-item-card">
-              <img
-                src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
-                class="menu-item-image" @click="vegStarterItems()"
-              />
-              <h1 class="menu-card-title">Non-Veg Starters</h1>
-              <a href="" class="menu-item-link">
-                View All
-              </a>
-            </div>
+          <div class="menu-item-card">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/em-ginger-fried-img.png"
+              class="menu-item-image"
+              @click="vegStarterItems()"
+            />
+            <h1 class="menu-card-title">Non-Veg Starters</h1>
+            <a href="" class="menu-item-link"> View All </a>
+          </div>
         </div>
         <div class="col-3 row justify-center">
-            <div class="menu-item-card">
-              <img
-                src="https://image.winudf.com/v2/image/Y29tLnl0b2ZmbGluZWJpcnlhbmlfc2NyZWVuXzE1XzE1MTU4ODY4MjNfMDEz/screen-15.jpg?fakeurl=1&type=.jpg"
-                class="menu-item-image" @click="menuItems()"
-              />
-              <h1 class="menu-card-title">Non-Veg Biryani</h1>
-              <a href="" class="menu-item-link">
-                View All
-              </a>
-            </div>
+          <div class="menu-item-card">
+            <img
+              src="https://image.winudf.com/v2/image/Y29tLnl0b2ZmbGluZWJpcnlhbmlfc2NyZWVuXzE1XzE1MTU4ODY4MjNfMDEz/screen-15.jpg?fakeurl=1&type=.jpg"
+              class="menu-item-image"
+              @click="menuItems()"
+            />
+            <h1 class="menu-card-title">Non-Veg Biryani</h1>
+            <a href="" class="menu-item-link"> View All </a>
+          </div>
         </div>
         <div class="col-3 row justify-center">
-            <div class="menu-item-card">
-              <img
-                src="https://th.bing.com/th/id/OIP.7iD-TDY26RsEwb-NQAjSkAHaE8?pid=ImgDet&rs=1"
-                class="menu-item-image" @click="menuItems()"
-              />
-              <h1 class="menu-card-title">Veg Biryani</h1>
-              <a href="" class="menu-item-link">
-                View All
-              </a>
-            </div>
+          <div class="menu-item-card">
+            <img
+              src="https://th.bing.com/th/id/OIP.7iD-TDY26RsEwb-NQAjSkAHaE8?pid=ImgDet&rs=1"
+              class="menu-item-image"
+              @click="menuItems()"
+            />
+            <h1 class="menu-card-title">Veg Biryani</h1>
+            <a href="" class="menu-item-link"> View All </a>
+          </div>
         </div>
+      </div>
+    </div>
+    <div>
+      <footer-section></footer-section>
     </div>
   </div>
 </template>
 
 <script>
+import FooterSection from './FooterSection.vue'
 export default {
   name: 'MenuSection',
   methods: {
@@ -66,20 +68,23 @@ export default {
     vegStarterItems () {
       this.$router.push('/vegstarteritems')
     }
+  },
+  components: {
+    FooterSection
   }
 }
 </script>
 
 <style>
-.menu-item-card{
-    border-radius: 16px;
+.menu-item-card {
+  border-radius: 16px;
 }
-.menu-item-image{
-    border-radius: 16px;
-    width: 100px;
-    height: 100px;
+.menu-item-image {
+  border-radius: 16px;
+  width: 100px;
+  height: 100px;
 }
-.menu-card-title{
+.menu-card-title {
   color: #323f4b;
   font-family: "Roboto";
   font-size: 18px;
